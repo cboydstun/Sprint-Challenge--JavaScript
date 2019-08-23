e3f6639fe02aae88eddd312542984a29a5fc1fa9
cboydstun <cboydstun@users.noreply.github.com>
2019-08-23 11:08:56 -0500

    functions done

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume (a, b, cb){
+  return cb(a, b);
+};
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,29 +17,41 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a, b){
+  return a + b;
+}
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function greeting (firstName, lastName){
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+};
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: Variables can fall into functions, but they can't fall out. 
 
 
-const external = "I'm outside the function";
+const external = "I'm outside the function"; // gloabl scope
 
-function myFunction() {
+function myFunction() { // specific scope
   console.log(external);
   const internal = "Hello! I'm inside myFunction!";
 
-  function nestedFunction() {
+  function nestedFunction() { // nested in the function
     console.log(internal);
   };
-  nestedFunction();
+  nestedFunction(); // calls the function
 }
-myFunction();
+myFunction(); // still way outside
